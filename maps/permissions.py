1a8d1bad11b79d6b69c1bc33ec11c83b5e522e52
pmv-pmv <pisarenkomariia@gmail.com>
2026-02-23 13:10:50 +0200

    fix:INIT-87 change `role` values for JWT accordance

--- maps/permissions.py
+++ maps/permissions.py
@@ -1,14 +1,14 @@
 from rest_framework.permissions import BasePermission
-
-MAP_ALLOWED_ROLES = ['viewer', 'editor', 'admin']
+# 1 - viewer; 2 - editor (silver); 3 - admin (gold)
+MAP_ALLOWED_ROLES = [1, 2, 3]
 
 class IsEditor(BasePermission):
     def has_permission(self, request, view):
-        return request.user_role in ['editor', 'admin']
+        return request.user_role in [2, 3]
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user_role == 'admin'
+        return request.user_role == 3
 
 class HasMapAccess(BasePermission):
     def has_permission(self, request, view):
